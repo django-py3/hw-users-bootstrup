68e76eb80321017106a2404ecb08bcacc57c1b9f
Django Py3 <admin@cpnc.ru>
2020-12-24 15:37:23 +0300

    HW+Users with bootstrup commit 1

---
+++ users/forms.py
@@ -0,0 +1,24 @@
+from .models import RegularUser
+
+# Заготовки интерфейса пользовательской модели
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+class RegularUserCreationForm(UserCreationForm):
+    """
+    Наш интерфейс взаимодействия с нашей CustomUser моделью
+    Создание юзера
+    """
+    class Meta(UserCreationForm.Meta):
+        model = RegularUser
+        fields = UserCreationForm.Meta.fields + ('age',) # При создании пользователя еще хотим указывать
+                                                        # и возраст
+        fields = UserCreationForm.Meta.fields + ('phone_number',)
+
+class RegularUserChangeForm(UserChangeForm):
+    """
+    Наш интерфейс взаимодействия с нашей CustomUser моделью
+    Редактирование юзера
+    """
+    class Meta(UserChangeForm.Meta):
+        model = RegularUser
+        fields = UserChangeForm.Meta.fields 
